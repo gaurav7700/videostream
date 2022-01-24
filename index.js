0ce4a39dfb2edf03e2aa22bf2af2e406d6054b60
gaurav chauhan <rgbpremium@gmail.com>
2022-01-25 01:57:47 +1100

    make it better

--- index.js
+++ index.js
@@ -53,10 +53,15 @@ app.use(function (req, res, next) {
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
+
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+  app.get("/upload", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  
 }
 
 const storage = multer.diskStorage({
