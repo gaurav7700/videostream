b09ec58c8154ee21431c585af965bbf15cd0ec77
gaurav chauhan <rgbpremium@gmail.com>
2022-01-25 01:22:09 +1100

    make it better

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ con.connect(function (err) {
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://videonow.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
